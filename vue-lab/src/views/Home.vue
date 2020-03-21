119effa6f2d522c3b1216c199f59989260a478f7
tmk-hsn <tomoki213@gmail.com>
2020-03-22 04:59:10 +0900

    テキストバインディング

--- vue-lab/src/views/Home.vue
+++ vue-lab/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>テキストバインディング</h3>
+    <span>{{ msg }}</span>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+export default Vue.extend({
+  data() {
+    return {
+      msg: "Hello Vue.js!"
+    };
   }
-}
+});
 </script>
+
+<style scoped>
+.home {
+  text-align: left;
+}
+</style>
